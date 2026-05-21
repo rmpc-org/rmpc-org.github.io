ec33bfef0a4ada9055a3c8657ceb6c5d54da5078
v-Nyo <135179386+v-Nyo@users.noreply.github.com>
2026-05-21 18:33:57 +0000

    docs: update nord (#62)

--- src/assets/themes/nord/sed.sh
+++ src/assets/themes/nord/sed.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env sh
-echo "Note: cava theme config requires hex values. The nord theme will fail if you run this script without manually updating those values."
-sed -i 's/#81a1c1/blue/gI; s/#88c0d0/cyan/g; s/#2e3440/black/g; s/#3b4252/black/g; s/#4c566a/gray/g; \
-        s/#a3be8c/green/g; s/#ebcb8b/yellow/g; s/#d8dee9/white/g; s/#b48ead/magenta/g'
+echo "Note: cava theme config requires hex values. The nord theme will fail if you run this without manually updating those values."
+sed -i 's/#81a1c1/blue/gI; s/#88c0d0/cyan/gI; s/#2e3440/black/gI; s/#3b4252/black/gI; s/#4c566a/gray/gI; s/#a3be8c/green/gI; s/#ebcb8b/yellow/gI; s/#d8dee9/white/gI; s/#b48ead/magenta/gI ; s/#d08770/yellow/gI; s/#bf616a/red/gI'
